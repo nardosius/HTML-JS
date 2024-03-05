6d5299b0f2406c7a2706e87b4d87a2b2be8c96fc
nardosius <nixonbii3@gmail.com>
2024-03-05 16:58:53 -0600

    more styled components

--- css-starting-project-01/src/components/AuthInputs.jsx
+++ css-starting-project-01/src/components/AuthInputs.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { styled } from 'styled-components';
 
+/*The following are examples of styled components to be passed into elements as in-line styles. Could be good for very small web apps/pages*/
 const ControlDiv = styled.div`
 display: flex;
   flex-direction: column;
   gap: 0.5rem;
   margin-bottom: 1.5rem;
-`
+`;
 const Label = styled.label`
   display: block;
   margin-bottom: 0.5rem;
@@ -14,19 +15,34 @@ const Label = styled.label`
   font-weight: 700;
   letter-spacing: 0.1em;
   text-transform: uppercase;
-  color: #6b7280;
-`
+  color: ${({$invalid}) => $invalid ? `#f87171` : `#6b7280`}; 
+`;
 
 const Input = styled.input`
 width: 100%;
 padding: 0.75rem 1rem;
 line-height: 1.5;
-background-color: #d1d5db;
-color: #374151;
-border: 1px solid transparent;
+background-color: ${({$invalid}) => ($invalid ? `#fed2d2` : `#d1d5db`)};
+color: ${({$invalid}) => ($invalid ? `#ef4444` : `#374151`)};
+border: 1px solid ${($invalid) => ($invalid ? `#f73f3f` : transparent)};
 border-radius: 0.25rem;
 box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`
+`;
+
+const Button = styled.button`
+  padding: 1rem 2rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  border-radius: 0.25rem;
+  color: #1f2937;
+  background-color: #f0b322;
+  border-radius: 6px;
+  border: none;
+
+  &:hover {
+    background-color: #f0920e;
+  }
+`;
 
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState('');
@@ -52,8 +68,9 @@ export default function AuthInputs() {
     <div id="auth-inputs">
       <ControlDiv>
         <p>
-          <Label className={`label ${emailNotValid ? 'invalid' : ''}`}>Email</Label>
+          <Label $invalid={emailNotValid}>Email</Label>
           <Input
+            $invalid={emailNotValid}
             type="email"
             // style={{
             //   backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db'
@@ -63,10 +80,11 @@ export default function AuthInputs() {
           />
         </p>
         <p>
-          <Label>Password</Label>
+          <Label $invalid={passwordNotValid}>Password</Label>
           <Input
+            $invalid={passwordNotValid}
             type="password"
-            className={`label ${passwordNotValid ? 'invalid' : ''}`}
+            className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
@@ -77,7 +95,7 @@ export default function AuthInputs() {
         <button type="button" className="text-button">
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
